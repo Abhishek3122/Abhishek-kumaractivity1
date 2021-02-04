2465537bc9dcf8b5b89fa2954e853e1f05deab28
Abhishek3122 <66154483+Abhishek3122@users.noreply.github.com>
2021-02-04 17:00:13 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -5,7 +5,7 @@ int a,b,c;
 printf("the numbers a and b\n");
 scanf("%d%d",&a,&b);
 c=a+b;
-printf("the sum of a and b is %d\n",c);
+printf("the sum of 1 and 2 is %d\n",c);
 return 0;
 }
 //n is mssing in 5 th line and 8 th line
